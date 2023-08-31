a3404a03333b3bb8224dd3ba6888c92f67bb0c34
Nikhil Raj <112814295+gantanikhilraj@users.noreply.github.com>
2023-08-31 23:57:52 +0530

    firebase, npm tools added for hosting

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_recommendation_app/screens/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: "AIzaSyBYZ-Ii8f9DxlpTez2h-_KOiC1LlMQSFPg",
+    projectId: "moviefeedapp",
+    messagingSenderId: "548119300880",
+    appId: "1:548119300880:web:53a9ff2df469169a2c3395",
+  ));
+
   runApp(const MyApp());
 }
 
